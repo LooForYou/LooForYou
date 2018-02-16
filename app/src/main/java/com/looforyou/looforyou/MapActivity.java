fbdf95fe5fc7ef1fa3461829c6babb6e85f99ed7
Mingtau Li <minijordon@gmail.com>
2018-02-16 14:39:47 -0800

    added tabs on bottom

--- app/src/main/java/com/looforyou/looforyou/MapActivity.java
+++ app/src/main/java/com/looforyou/looforyou/MapActivity.java
@@ -1,6 +1,5 @@
 package com.looforyou.looforyou;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
@@ -9,42 +8,37 @@ import android.os.Bundle;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
 
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-        bottomBar.selectTabWithId(R.id.tab_home);
+        bottomBar.selectTabWithId(R.id.tab_map);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
                 switch(tabId) {
+                    case R.id.tab_home:
+                         Intent homeIntent = new Intent(MapActivity.this, MainActivity.class);
+                        startActivity(homeIntent);
+                        break;
                     case R.id.tab_profile:
-                        Intent profileIntent = new Intent(MainActivity.this, ProfileActivity.class);
+                        Intent profileIntent = new Intent(MapActivity.this, ProfileActivity.class);
                         startActivity(profileIntent);
                         break;
                     case R.id.tab_add_bathroom:
-                        Intent AddIntent = new Intent(MainActivity.this, AddABathroomActivity.class);
+                        Intent AddIntent = new Intent(MapActivity.this, AddABathroomActivity.class);
                         startActivity(AddIntent);
                         break;
-                    case R.id.tab_map:
-                        Intent mapIntent = new Intent(MainActivity.this, MapActivity.class);
-                        startActivity(mapIntent);
-                        break;
                     case R.id.tab_bookmarks:
-                        Intent bookMarksIntent = new Intent(MainActivity.this, BookmarkActivity.class);
+                        Intent bookMarksIntent = new Intent(MapActivity.this, BookmarkActivity.class);
                         startActivity(bookMarksIntent);
 
                 }
             }
         });
-
-
     }
-
-
-
 }
