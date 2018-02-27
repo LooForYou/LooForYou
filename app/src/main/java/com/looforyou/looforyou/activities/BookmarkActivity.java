58bc8551e1efd254258184e6c6c4688ce6695229
Mingtau Li <minijordon@gmail.com>
2018-02-26 21:31:55 -0800

    added current location functionality to map fragment

--- app/src/main/java/com/looforyou/looforyou/activities/BookmarkActivity.java
+++ app/src/main/java/com/looforyou/looforyou/activities/BookmarkActivity.java
@@ -1,8 +1,27 @@
 package com.looforyou.looforyou.activities;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ActionMenuView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.looforyou.looforyou.R;
 import com.looforyou.looforyou.utilities.TabControl;
 
@@ -15,4 +34,5 @@ public class BookmarkActivity extends AppCompatActivity {
         TabControl tabb = new TabControl(this);
         tabb.tabs(BookmarkActivity.this,R.id.tab_bookmarks);
     }
+
 }
