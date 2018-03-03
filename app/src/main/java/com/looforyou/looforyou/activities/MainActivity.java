38ac1ac784f55807a7011f5ef6f1dafa433a7de9
Mingtau Li <minijordon@gmail.com>
2018-03-03 02:04:50 -0800

    updated toolbars
    simplified toolbar layouts, added new directions icon, changed app theme for custom toolbar, converted menu items to layouts

--- app/src/main/java/com/looforyou/looforyou/activities/MainActivity.java
+++ app/src/main/java/com/looforyou/looforyou/activities/MainActivity.java
@@ -1,64 +1,38 @@
 package com.looforyou.looforyou.activities;
 
-import android.app.Activity;
-import android.app.SearchManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ActionMenuView;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.util.TypedValue;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.AutocompleteFilter;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
-import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.looforyou.looforyou.R;
-import com.looforyou.looforyou.utilities.MetricConverter;
 import com.looforyou.looforyou.utilities.TabControl;
-
-import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private ActionMenuView actionMenu;
     private final String GMAPS_TAG = "GException";
     private int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+    private View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //replace default actionbar with custom toolbar layout
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false); //get rid of title
-
-        //set click listener on custom toolbar menu
-        actionMenu = (ActionMenuView) toolbar.findViewById(R.id.leftMenu);
-        actionMenu.setOnMenuItemClickListener(new ActionMenuView.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                return onOptionsItemSelected(item);
-            }
-        });
+        showActionBar();
+        bindActionBar();
 
         //tabs on bottom control
         TabControl tabb = new TabControl(this);
@@ -66,127 +40,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //when options in toolbar menu are created
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, actionMenu.getMenu());
+    private void showActionBar() {
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setCustomView(R.layout.action_bar_main);
+        view = getSupportActionBar().getCustomView();
+    }
 
-        actionMenu.measure(0,0);
-        float menuWidth = actionMenu.getMeasuredWidth(); //get height
-//        item.getActionView().measure(0,0);
-//        int width = menu.getItem(0).getActionView().getWidth();
-//        Log.v(GMAPS_TAG,String.valueOf(width));
+    public void bindActionBar(){
+        ImageButton profileButton= (ImageButton)view.findViewById(R.id.action_bar_profile_main);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Log.v("Custom Tag","profile button clicked");
+                Toast.makeText(MainActivity.this,"profile...", Toast.LENGTH_SHORT).show();
 
-/* test get screen width */
-//        DisplayMetrics disp = new DisplayMetrics();
-//        (this).getWindowManager().getDefaultDisplay().getMetrics(disp);
-//        Resources r = getResources();
-//        float d = disp.widthPixels;
-//        float d2 = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, d, disp);
-//
-//        float converted = pxToDp(this.menuWidth);
-//        float converted2 = dpToPx(this,menuWidth);
-//        Log.v(GMAPS_TAG,String.valueOf(d2));
-//        Log.v(GMAPS_TAG,String.valueOf(converted));
-//        Log.v(GMAPS_TAG,String.valueOf(converted2));
+            }
+        });
 
-        TextView textView = (TextView) findViewById(R.id.custom_search_layout);
-        ViewGroup.LayoutParams params = (ViewGroup.LayoutParams) textView.getLayoutParams();
-        params.width = (int) MetricConverter.dpToPx(this,175);
-        textView.setLayoutParams(params);
-
-        //look for custom search bar actionLayout
-        for(int i = 0; i< actionMenu.getMenu().size();i++){
-            final MenuItem menuItem = actionMenu.getMenu().getItem(i);
-            if(menuItem.getItemId() == R.id.action_google_search){
-                View view = menuItem.getActionView();
-                if(view != null){
-                    //set listener on searchbar
-                    view.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-
-                            
-                            try {
-                                //start autoplaces search
-                                Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).build(MainActivity.this);
-                                startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
-                            } catch (GooglePlayServicesRepairableException e) {
-                                Log.v(GMAPS_TAG, "repairableException " + e);
-                            } catch (GooglePlayServicesNotAvailableException e) {
-                                // TODO: Handle the error.
-                                Log.v(GMAPS_TAG, "notAvailableException " + e);
-                            }
-
-/*                            menuItem.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-                                @Override
-                                public void onPlaceSelected(Place place) {
-                                    // TODO: Get info about the selected place.
-                                    Log.i(TAG, "Place: " + place.getName());//get place details here
-                                }
-
-                                @Override
-                                public void onError(Status status) {
-                                    // TODO: Handle the error.
-                                    Log.i(TAG, "An error occurred: " + status);
-                                }
-                            });*/
-
-                        }
-                    });
+        final LinearLayout searchBar= (LinearLayout)view.findViewById(R.id.action_bar_search_main);
+        searchBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    //start autoplaces search
+                    Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).build(MainActivity.this);
+                    startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+                } catch (GooglePlayServicesRepairableException e) {
+                    Log.v(GMAPS_TAG, "repairableException " + e);
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    // TODO: Handle the error.
+                    Log.v(GMAPS_TAG, "notAvailableException " + e);
                 }
             }
-        }
-//        return true;
-        return super.onCreateOptionsMenu(menu);
-    }
+        });
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-     //   actionMenu.measure(0,0);
-
-
-        int id = item.getItemId();
-        Toast.makeText(this,item.getTitle(), Toast.LENGTH_SHORT);
-        if(id == R.id.action_google_search) {
-            int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
-            String GMAPS_TAG = "GException";
-            try {
-                Intent intent =
-                        new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).build(this);
-//                        new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY).build(this);
-                startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
-
-                //test filter
-//                AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
-//                        .setTypeFilter(AutocompleteFilter.TYPE_FILTER_ADDRESS)
-//                        .build();
-
-
-            } catch (GooglePlayServicesRepairableException e) {
-                Toast.makeText(this, "repairableException", Toast.LENGTH_SHORT);
-                Log.v(GMAPS_TAG, "repairableException " + e);
-            } catch (GooglePlayServicesNotAvailableException e) {
-                // TODO: Handle the error.
-                Toast.makeText(this, "ServicesNotAvailableException", Toast.LENGTH_SHORT);
-                Log.v(GMAPS_TAG, "notAvailableException " + e);
+        ImageButton sortButton= (ImageButton)view.findViewById(R.id.action_bar_sort_main);
+        sortButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Log.v("Custom Tag","sort clicked");
+                Toast.makeText(MainActivity.this,"sort by...", Toast.LENGTH_SHORT).show();
             }
-        }
-//        }else {
-        Log.v("GException",String.valueOf(item));
-        return super.onOptionsItemSelected(item);
+        });
 
+        ImageButton listViewButton= (ImageButton)view.findViewById(R.id.action_bar_list_view_main);
+        listViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Log.v("Custom Tag","view list");
+                Toast.makeText(MainActivity.this,"view list...", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-    /* end menu*/
 
-
+    /* results from google search*/
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        //autocompleteFragment.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 Place place = PlaceAutocomplete.getPlace(this, data);
