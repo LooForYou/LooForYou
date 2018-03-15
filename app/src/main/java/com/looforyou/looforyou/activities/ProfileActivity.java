66a81ec9dd1853453a2ba847706c09d2bb104401
pgrim711 <Phoenixx0711@hotmail.com>
2018-03-14 21:24:33 -0700

    Dialog popup

--- app/src/main/java/com/looforyou/looforyou/activities/ProfileActivity.java
+++ app/src/main/java/com/looforyou/looforyou/activities/ProfileActivity.java
@@ -1,30 +1,41 @@
 package com.looforyou.looforyou.activities;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.looforyou.looforyou.R;
 import com.looforyou.looforyou.utilities.TabControl;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        myDialog = new Dialog(this);
 
         TabControl tabb = new TabControl(this);
         tabb.tabs(ProfileActivity.this,R.id.tab_profile);
 
-        Button b = (Button) findViewById(R.id.blogin);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(ProfileActivity.this, Pop.class));
-            }
-        });
+        };
+
+        public void ShowPopup(View v){
+            TextView txtclose;
+            myDialog.setContentView(R.layout.activity_login);
+            txtclose = (TextView) myDialog.findViewById(R.id.txtclose);
+            txtclose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    myDialog.dismiss();
+                }
+            });
+            myDialog.show();
     }
 }
+
