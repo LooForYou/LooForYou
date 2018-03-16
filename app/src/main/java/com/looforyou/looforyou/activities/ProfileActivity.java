6549d4efde25e15a0fc9c5c178025f2a0392277d
pBouris <pbouris6@yahoo.com>
2018-03-16 12:12:23 -0700

    updated profile, bookmark, and add bathroom pages

--- app/src/main/java/com/looforyou/looforyou/activities/ProfileActivity.java
+++ app/src/main/java/com/looforyou/looforyou/activities/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,7 +20,7 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         myDialog = new Dialog(this);
-
+        Log.v("mylog","this is a test log");
         TabControl tabb = new TabControl(this);
         tabb.tabs(ProfileActivity.this,R.id.tab_profile);
 
